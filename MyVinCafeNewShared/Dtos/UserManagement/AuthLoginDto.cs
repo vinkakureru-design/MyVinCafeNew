49d8774dfb560a96f2611291e28f97c6c8cf043f
Nadeshiko Chan <vinkakureru@gmail.com>
2026-04-17 08:27:42 +0700

    hapus migrasi dan drop database

---
+++ MyVinCafeNewShared/Dtos/UserManagement/AuthLoginDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace MyVinCafeNewShared.Dtos.UserManagement
+{
+    public class AuthLoginDto
+    {
+        [Required] public string UserName { get; set; } = null!;
+        [Required] public string Password { get; set; } = null!;
+    }
+}
---
+++ MyVinCafeNewShared/Dtos/UserManagement/AuthRegisterDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace MyVinCafeNewShared.Dtos.UserManagement
+{
+    public class AuthRegisterDto
+    {
+        public string? FullName { get; set; }
+        [Required] public string UserName { get; set; } = null!;
+        [Required] public string Password { get; set; } = null!;
+        [EmailAddress] public string EmailAddress { get; set; } = null!;
+        [Phone] public string PhoneNumber { get; set; } = null!;
+    }
+}
